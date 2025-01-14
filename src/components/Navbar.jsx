78b7a2682d71be7db548e694be1c7ae04b4d9821
jhon-Min <minnyisay@gmail.com>
2025-01-14 14:48:38 +0630

    ui: add mobile nav ui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
 import { NavLink } from 'react-router';
 import logo from '../../public/img/logo.png';
 import './Navbar.css';
-import { HiHome } from 'react-icons/hi';
+import {
+  HiHome,
+  HiOutlineViewBoards,
+  HiUser,
+  HiUserGroup,
+} from 'react-icons/hi';
 
 export default function Navbar() {
   return (
@@ -36,31 +41,55 @@ export default function Navbar() {
         </div>
       </nav>
 
-      <nav className="md:hidden fixed bottom-0 left-0 z-50 w-full h-[75px] bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
-        <div className="px-[15px] flex items-center justify-around">
-          <NavLink
-            to="/"
-            className="mr-[50px] hover:text-red-500 transcation-colors duration-300 ease-in-out"
-          >
-            <HiHome className="text-[30px] text-center" />
-            Home
-          </NavLink>
+      <nav className="md:hidden bg-[#101720] fixed bottom-0 left-0 z-50 w-full h-[72px] ">
+        <div className="grid grid-cols-4 gap-3 pt-3 ">
+          <div className="text-center">
+            <NavLink
+              to="/"
+              className="mr-[50px] text-[#434F5D] hover:text-red-500 transcation-colors duration-300 ease-in-out"
+            >
+              <div className="flex flex-col items-center">
+                <HiHome className="text-[27px] text-center" />
+                <p className="text-[16px]">Home</p>
+              </div>
+            </NavLink>
+          </div>
 
-          <NavLink
-            to="/"
-            className="mr-[50px] hover:text-red-500 transcation-colors duration-300 ease-in-out"
-          >
-            <HiHome className="text-[30px]" />
-            Home
-          </NavLink>
+          <div className="text-center">
+            <NavLink
+              to="/"
+              className="mr-[50px] text-[#434F5D] hover:text-red-500 transcation-colors duration-300 ease-in-out"
+            >
+              <div className="flex flex-col items-center">
+                <HiOutlineViewBoards className="text-[27px] text-center" />
+                <p className="text-[16px]">Books</p>
+              </div>
+            </NavLink>
+          </div>
 
-          <NavLink
-            to="/"
-            className="mr-[50px] hover:text-red-500 transcation-colors duration-300 ease-in-out"
-          >
-            <HiHome className="text-[30px]" />
-            Home
-          </NavLink>
+          <div className="text-center ">
+            <NavLink
+              to="/"
+              className="mr-[50px] text-[#434F5D] hover:text-red-500 transcation-colors duration-300 ease-in-out"
+            >
+              <div className="flex flex-col items-center">
+                <HiUserGroup className="text-[27px] text-center" />
+                <p className="text-[16px]">About</p>
+              </div>
+            </NavLink>
+          </div>
+
+          <div className="text-center">
+            <NavLink
+              to="/"
+              className="mr-[50px] text-[#434F5D] hover:text-red-500 transcation-colors duration-300 ease-in-out"
+            >
+              <div className="flex flex-col items-center">
+                <HiUser className="text-[27px] text-center" />
+                <p className="text-[16px]">Login</p>
+              </div>
+            </NavLink>
+          </div>
         </div>
       </nav>
     </>
