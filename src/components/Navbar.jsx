f5062b7ea1158c70a1981e4a891726c8d103622d
jhon-Min <minnyisay@gmail.com>
2025-03-22 17:33:49 +0630

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,23 +28,46 @@ export default function Navbar() {
         <div className=" text-white text-[20px] hidden md:block">
           <NavLink
             to="/"
-            className="mr-[50px] hover:text-red-500 transcation-colors duration-300 ease-in-out"
+            className={({ isActive }) =>
+              `mr-[50px] ${
+                isActive ? 'text-red-500' : 'text-white'
+              } hover:text-red-500 transition-colors duration-300 ease-in-out`
+            }
           >
             Home
           </NavLink>
 
           <NavLink
             to="/about"
-            className="mr-[50px] hover:text-red-500 transcation-colors duration-300 ease-in-out"
+            className={({ isActive }) =>
+              `mr-[50px] ${
+                isActive ? 'text-red-500' : 'text-white'
+              } hover:text-red-500 transition-colors duration-300 ease-in-out`
+            }
           >
             About
           </NavLink>
 
           {token ? (
+            <NavLink
+              to="/profile"
+              className={({ isActive }) =>
+                `mr-[50px] ${
+                  isActive ? 'text-red-500' : 'text-white'
+                } hover:text-red-500 transition-colors duration-300 ease-in-out`
+              }
+            >
+              Profile
+            </NavLink>
+          ) : (
+            ''
+          )}
+
+          {token ? (
             <button
               type="button"
               onClick={() => logoutHandler()}
-              class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-[16px] px-6 py-2.5 text-center"
+              class="text-white ms-5 bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-[16px] px-6 py-2.5 text-center"
             >
               Logout
             </button>
